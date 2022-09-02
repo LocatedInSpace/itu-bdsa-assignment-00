2c2be846adfbd5cb2c4b7c11519723c05758dbc6
LocatedInSpace <48300341+LocatedInSpace@users.noreply.github.com>
2022-09-02 13:51:24 +0200

    Tested and implemented IsLeapYear

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,3 +1,5 @@
+using ConsoleApp;
+
 namespace TestProject;
 
 public class UnitTest1
@@ -5,5 +7,23 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var year = new Program();
+        Assert.True(year.IsLeapYear(4));
+        Assert.True(year.IsLeapYear(8));
+        Assert.False(year.IsLeapYear(-1));
+        Assert.False(year.IsLeapYear(9));
+        Assert.True(year.IsLeapYear(0));
+        Assert.True(year.IsLeapYear(-400));
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var year = new Program();
+        Assert.True(year.IsLeapYear(96));
+        Assert.False(year.IsLeapYear(-100));
+        Assert.False(year.IsLeapYear(300));
+        Assert.True(year.IsLeapYear(400));
+        Assert.False(year.IsLeapYear(1521));
     }
 }
